119dbff5bd77af494ee9ef83bd9106c7a131a266
Harshit <168411720+slash-aech@users.noreply.github.com>
2025-06-11 01:28:51 +0530

    Update index.js

--- dashboards/server/index.js
+++ dashboards/server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://facial-recognition-attendance-3wlw.onrender.com',
+  origin: 'https://onrender.com',
   credentials: true
 }));
 app.use(express.json());
