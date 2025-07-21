30455bffb360a045205df82dd998c58bc7c9b78b
Harshit <168411720+slash-aech@users.noreply.github.com>
2025-07-21 19:02:09 +0530

    removed excess files

--- face login-register/server/routers/faceRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { checkFaceRegistration, registerFace } = require('../controllers/faceController');
-
-router.post('/check', checkFaceRegistration);
-router.post('/register', registerFace)
-
-module.exports = router;
--- face login-register/server/routers/userRouters.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUserById, changePasswordByUserInfoId, faceLogin } = require('../controllers/userController');
-
-router.get('/user/:id', getUserById);
-router.post('/user/changePassword', changePasswordByUserInfoId)
-router.post('/user/face-login', faceLogin);
-
-module.exports = router;
